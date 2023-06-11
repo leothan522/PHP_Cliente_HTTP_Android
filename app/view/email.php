16d4ddba758ea46ef7abcb75d0dfdee6dba0fcc5
Yonathan Castillo <leothan522@gmail.com>
2023-06-11 00:27:49 -0400

    Enlace de Restablecimiento de Contraseña

---
+++ app/view/email.php
@@ -0,0 +1,147 @@
+<table width="100%" cellspacing="0" cellpadding="12" border="0">
+    <tbody>
+    <tr>
+        <td>
+            <div style="overflow: hidden;"><font size="-1"><u></u>
+                    <div style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';background-color:#ffffff;color:#718096;height:100%;line-height:1.4;margin:0;padding:0;width:100%!important">
+                        <table role="presentation"
+                               style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';background-color:#edf2f7;margin:0;padding:0;width:100%"
+                               width="100%" cellspacing="0" cellpadding="0">
+                            <tbody>
+                            <tr>
+                                <td style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'"
+                                    align="center">
+                                    <table role="presentation"
+                                           style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';margin:0;padding:0;width:100%"
+                                           width="100%" cellspacing="0" cellpadding="0">
+                                        <tbody>
+                                        <tr>
+                                            <td style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';padding:25px 0;text-align:center">
+                                                <a href="%APP_URL%"
+                                                   style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';color:#3d4852;font-size:19px;font-weight:bold;text-decoration:none;display:inline-block"
+                                                   target="_blank">
+                                                    <!--Morros Devops-->
+                                                    %APP_NAME%
+                                                </a>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td cellpadding="0" cellspacing="0"
+                                                style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';background-color:#edf2f7;border-bottom:1px solid #edf2f7;border-top:1px solid #edf2f7;margin:0;padding:0;width:100%;border:hidden!important"
+                                                width="100%">
+                                                <table class="m_7296488423494989454inner-body" role="presentation"
+                                                       style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';background-color:#ffffff;border-color:#e8e5ef;border-radius:2px;border-width:1px;margin:0 auto;padding:0;width:570px"
+                                                       width="570" cellspacing="0" cellpadding="0" align="center">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';max-width:100vw;padding:32px">
+                                                            <h1 style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';color:#3d4852;font-size:18px;font-weight:bold;margin-top:0;text-align:left">
+                                                                ¡Hola!</h1>
+                                                            <p style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';font-size:16px;line-height:1.5em;margin-top:0;text-align:left">
+                                                                Ha recibido este mensaje porque se solicitó un
+                                                                restablecimiento de contraseña para su cuenta.</p>
+                                                            <table role="presentation"
+                                                                   style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';margin:30px auto;padding:0;text-align:center;width:100%"
+                                                                   width="100%" cellspacing="0" cellpadding="0"
+                                                                   align="center">
+                                                                <tbody>
+                                                                <tr>
+                                                                    <td style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'"
+                                                                        align="center">
+                                                                        <table role="presentation"
+                                                                               style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'"
+                                                                               width="100%" cellspacing="0"
+                                                                               cellpadding="0" border="0">
+                                                                            <tbody>
+                                                                            <tr>
+                                                                                <td style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'"
+                                                                                    align="center">
+                                                                                    <table role="presentation"
+                                                                                           style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'"
+                                                                                           cellspacing="0"
+                                                                                           cellpadding="0" border="0">
+                                                                                        <tbody>
+                                                                                        <tr>
+                                                                                            <td style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'">
+                                                                                                <a href="%URL_RECUPERAR%"
+                                                                                                   class="m_7296488423494989454button"
+                                                                                                   rel="noopener"
+                                                                                                   style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';border-radius:4px;color:#fff;display:inline-block;overflow:hidden;text-decoration:none;background-color:#2d3748;border-bottom:8px solid #2d3748;border-left:18px solid #2d3748;border-right:18px solid #2d3748;border-top:8px solid #2d3748"
+                                                                                                   target="_blank">
+                                                                                                    Restablecer contraseña</a>
+                                                                                            </td>
+                                                                                        </tr>
+                                                                                        </tbody>
+                                                                                    </table>
+                                                                                </td>
+                                                                            </tr>
+                                                                            </tbody>
+                                                                        </table>
+                                                                    </td>
+                                                                </tr>
+                                                                </tbody>
+                                                            </table>
+                                                            <p style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';font-size:16px;line-height:1.5em;margin-top:0;text-align:left">
+                                                                Este enlace de restablecimiento de contraseña expirará
+                                                                en 60 minutos.</p>
+                                                            <p style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';font-size:16px;line-height:1.5em;margin-top:0;text-align:left">
+                                                                Si no ha solicitado el restablecimiento de contraseña,
+                                                                omita este mensaje de correo electrónico.</p>
+                                                            <p style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';font-size:16px;line-height:1.5em;margin-top:0;text-align:left">
+                                                                Saludos,<br>
+                                                                Morros Devops</p>
+                                                            <table role="presentation"
+                                                                   style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';border-top:1px solid #e8e5ef;margin-top:25px;padding-top:25px"
+                                                                   width="100%" cellspacing="0" cellpadding="0">
+                                                                <tbody>
+                                                                <tr>
+                                                                    <td style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'">
+                                                                        <p style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';line-height:1.5em;margin-top:0;text-align:left;font-size:14px">
+                                                                            Si está teniendo problemas al hacer clic en
+                                                                            el botón "Restablecer contraseña", copie y
+                                                                            pegue la URL de abajo
+                                                                            en su navegador web: <span
+                                                                                    style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';word-break:break-all"><a
+                                                                                        href="%URL_RECUPERAR%"
+                                                                                        style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';color:#3869d4"
+                                                                                        target="_blank">%URL_RECUPERAR%</a></span>
+                                                                        </p>
+                                                                    </td>
+                                                                </tr>
+                                                                </tbody>
+                                                            </table>
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol'">
+                                                <table class="m_7296488423494989454footer" role="presentation"
+                                                       style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';margin:0 auto;padding:0;text-align:center;width:570px"
+                                                       width="570" cellspacing="0" cellpadding="0" align="center">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';max-width:100vw;padding:32px"
+                                                            align="center">
+                                                            <p style="box-sizing:border-box;font-family:-apple-system,BlinkMacSystemFont,'Segoe UI',Roboto,Helvetica,Arial,sans-serif,'Apple Color Emoji','Segoe UI Emoji','Segoe UI Symbol';line-height:1.5em;margin-top:0;color:#b0adc5;font-size:12px;text-align:center">
+                                                                © 2023 %APP_NAME%. Todos los derechos reservados.</p>
+                                                        </td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </font></div>
+        </td>
+    </tr>
+    </tbody>
+</table>
--- recuperar.php
+++ recuperar.php
@@ -2,9 +2,12 @@
 session_start();
 require_once "vendor/autoload.php";
 
+use Dotenv\Dotenv;
 use model\User;
 use controller\MailerController;
 
+$dotenv = Dotenv::createImmutable('../laravel');
+$dotenv->load();
 
 $response = array();
 
@@ -19,24 +22,43 @@ if ($_POST) {
             $email = $_POST['email'];
 
             $existe = $users->existe('email', '=', $email);
-            if ($existe){
+            if ($existe) {
 
-                $password = generateString(8);
+                //$password = generateString(8);
+                $token = generateString(60);
+                $email_url = str_replace('@', '%40', $email);
+                $url_recuperar = $_ENV['APP_URL'] . '/recuperar/' . $token . '/' . $email_url;
+                $hoy = date("Y-m-d H:i:s");
 
                 $mailer = new MailerController();
-                $asunto = 'Nuevo Password';
-                $mensaje = 'Hola, este es tu nuevo Password: <h4 style="color: blue">'.$password.'</h4> Asegurate de guardar bien la clave.';
-                $noHTML = "Este es un mensaje para los clientes que no soportan HTML. Nuevo Password: $password";
+                $asunto = utf8_decode('Notificación de restablecimiento de contraseña');
+                //$mensaje = 'Hola, este es tu nuevo Password: <h4 style="color: blue">'.$password.'</h4> Asegurate de guardar bien la clave.';
+                $mensaje = file_get_contents('app/view/email.php', FILE_USE_INCLUDE_PATH);
+                $mensaje = str_replace('%APP_URL%', $_ENV['APP_URL'], $mensaje);
+                $mensaje = str_replace('%APP_NAME%', $_ENV['APP_NAME'], $mensaje);
+                $mensaje = str_replace('%URL_RECUPERAR%', $url_recuperar, $mensaje);
+
+                //$noHTML = "Este es un mensaje para los clientes que no soportan HTML. Nuevo Password: $password";
+                $texto = "Este es un mensaje para los clientes de que no soportan HTML. \n 
+                          Ha recibido este mensaje porque se solicitó un restablecimiento de contraseña para su cuenta.\n
+                          Copie y pegue la URL de abajo en su navegador web:\n
+                          ".$url_recuperar." \n
+                          Este enlace de restablecimiento de contraseña expirará en 60 minutos. \n
+                          Si no ha solicitado el restablecimiento de contraseña, omita este mensaje de correo electrónico. \n
+                          Saludos, \n
+                          ".$_ENV['APP_NAME'];
+                $noHTML = utf8_decode($texto);
                 $mailer->enviarEmail($email, $asunto, $mensaje, $noHTML);
 
-                $users->update($existe['id'], 'password', password_hash($password, PASSWORD_DEFAULT));
+                $users->update($existe['id'], 'token_recuperacion', $token);
+                $users->update($existe['id'], 'times_recuperacion', $hoy);
 
                 $response['result'] = true;
                 $response['icon'] = 'success';
-                $response['title'] = "Nueva Contraseña Enviada.";
-                $response['text'] = "La nueva clave fue enviada por correo: ".$email;
+                $response['title'] = "Enlace de restablecimiento Enviado.";
+                $response['text'] = "Le hemos enviado por correo electrónico el enlace para restablecer su contraseña.";
 
-            }else{
+            } else {
                 $response['result'] = false;
                 $response['icon'] = 'warning';
                 $response['title'] = "Email NO registrado";
